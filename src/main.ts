47c4a3ddb57dc7c2abeb44b4f7e6134a9eae9f89
Ignacio de Miguel Diaz <imigueldiaz@gmail.com>
2024-11-17 13:57:50 +0100

    refactor: Update labelerServer start port to 4100

--- src/main.ts
+++ src/main.ts
@@ -66,7 +66,7 @@ jetstream.onCreate(WANTED_COLLECTION, (event: CommitCreateEvent<typeof WANTED_CO
 
 const metricsServer = startMetricsServer(METRICS_PORT);
 
-labelerServer.start(PORT, (error, address) => {
+labelerServer.start(4100, (error, address) => {
   if (error) {
     logger.error('Error starting server: %s', error);
   } else {
